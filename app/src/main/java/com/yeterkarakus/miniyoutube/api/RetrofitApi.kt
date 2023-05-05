194875345d6757d2b8ec3b38a9821d0660eb6606
Yeter Karakus <yeterkarakus55@gmail.com>
2023-05-05 03:23:23 +0300

    Initial commit

--- app/src/main/java/com/yeterkarakus/miniyoutube/api/RetrofitApi.kt
+++ app/src/main/java/com/yeterkarakus/miniyoutube/api/RetrofitApi.kt
@@ -16,21 +16,21 @@ import retrofit2.http.Query
 
 interface RetrofitApi {
     @Headers(
-        "X-RapidAPI-Key: bd2f663c8amsh84ac99a19f918dbp1d11a7jsn37a234188bff",
-        "X-RapidAPI-Host: spotify23.p.rapidapi.com"
+        "X-RapidAPI-Key: ",
+        "X-RapidAPI-Host: "
     )
     @GET("artists/")
     suspend fun getArtist(@Query("ids") ids: String) : Response<ArtistContainer>
     @Headers(
-        "X-RapidAPI-Key: bd2f663c8amsh84ac99a19f918dbp1d11a7jsn37a234188bff",
-        "X-RapidAPI-Host: spotify23.p.rapidapi.com"
+        "X-RapidAPI-Key: ",
+        "X-RapidAPI-Host: "
     )
     @GET("artist_overview/")
     suspend fun getArtistOverview(@Query("id") id: String) : Response<ArtistOverviewContainer>
 
     @Headers(
-        "X-RapidAPI-Key: bd2f663c8amsh84ac99a19f918dbp1d11a7jsn37a234188bff",
-        "X-RapidAPI-Host: spotify23.p.rapidapi.com"
+        "X-RapidAPI-Key: ",
+        "X-RapidAPI-Host: "
     )
     @GET("search/")
     suspend fun search(@Query("q") q: String,
@@ -39,20 +39,20 @@ interface RetrofitApi {
                        @Query("limit") limit : Int = 3) : Response<SearchContainer>
 
     @Headers(
-        "X-RapidAPI-Key: bd2f663c8amsh84ac99a19f918dbp1d11a7jsn37a234188bff",
-        "X-RapidAPI-Host: spotify23.p.rapidapi.com"
+        "X-RapidAPI-Key: ",
+        "X-RapidAPI-Host: "
     )
     @GET("albums/")
     suspend fun getAlbums(@Query("ids") ids : String): Response<AlbumsContainer>
     @Headers(
-        "X-RapidAPI-Key: bd2f663c8amsh84ac99a19f918dbp1d11a7jsn37a234188bff",
-        "X-RapidAPI-Host: spotify23.p.rapidapi.com"
+        "X-RapidAPI-Key: ",
+        "X-RapidAPI-Host: "
     )
     @GET("tracks/")
     suspend fun getTrack(@Query("ids") ids : String): Response<TracksContainer>
     @Headers(
-        "X-RapidAPI-Key: bd2f663c8amsh84ac99a19f918dbp1d11a7jsn37a234188bff",
-        "X-RapidAPI-Host: spotify23.p.rapidapi.com"
+        "X-RapidAPI-Key: ",
+        "X-RapidAPI-Host: "
     )
     @GET("album_tracks/")
     suspend fun getAlbumTracks(@Query("id") id : String,
